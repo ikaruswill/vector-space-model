55e8d525d7e06deeb6450eb5706f1f4ccbb11e66
Will Ho <ikaruswill.ho@gmail.com>
2017-02-04 16:44:54 +0800

    Add missing imports, correct indentation

--- index.py
+++ index.py
@@ -1,24 +1,29 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+import getopt
+import sys
+import os
+import io
 
 
 def usage():
-    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+	print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
 if __name__ == '__main__':
 	dir_doc = dict_file = postings_file = None
 	try:
-	    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
-	except getopt.GetoptError as err:i
-	    usage()
-	    sys.exit(2)
+		opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+	except getopt.GetoptError as err:
+		usage()
+		sys.exit(2)
 	for o, a in opts:
-	    if o == '-i':
-	        dir_doc = a
-	    elif o == '-d':
-	        dict_file = a
-	    elif o == '-p':
-	        postings_file = a
-	    else:
-	        assert False, "unhandled option"
-	if input_file_b == None or input_file_t == None or output_file == None:
-	    usage()
-	    sys.exit(2)
+		if o == '-i':
+			dir_doc = a
+		elif o == '-d':
+			dict_file = a
+		elif o == '-p':
+			postings_file = a
+		else:
+			assert False, "unhandled option"
+	if dir_doc == None or dict_file == None or postings_file == None:
+		usage()
+		sys.exit(2)
