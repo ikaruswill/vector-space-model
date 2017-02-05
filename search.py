c63b132208222d128f6ff0c6079691239ef8e564
Will Ho <ikaruswill.ho@gmail.com>
2017-02-05 16:43:18 +0800

    Initial implementation of search.py

---
+++ search.py
@@ -0,0 +1,25 @@
+def usage():
+	print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
+if __name__ == '__main__':
+	dict_file = postings_file = query_file = output_file = None
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+	except getopt.GetoptError as err:
+		usage()
+		sys.exit(2)
+	for o, a in opts:
+		if o == '-d':
+			dict_file = a
+		elif o == '-p':
+			postings_file = a
+		elif o == '-q':
+			query_file = a
+		elif o == '-o':
+			output_file = a
+		else:
+			assert False, "unhandled option"
+	if dir_doc == None or dict_file == None or postings_file == None:
+		usage()
+		sys.exit(2)
+
