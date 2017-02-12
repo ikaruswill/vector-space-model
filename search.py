94661cf7325d60f9bfb98748da817b58b06b47c2
CCS <ccs6327@gmail.com>
2017-02-12 23:42:47 +0800

    basic search for search.py with unsolve bug

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import io
 import getopt
 import sys
 import pickle
+from nltk.stem.porter import PorterStemmer
 
 def usage():
 	print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
@@ -31,19 +32,50 @@ if __name__ == '__main__':
 	with io.open(dict_path, 'rb') as f:
 		dictionary = pickle.load(f)
 
+	for i in range(100):
+		print(dictionary[i])
+
 	# TODO: Implement seeking and reading don't read entirely
-	
+
 	# load postings object sizes to calculate seek offset from current position of file
 	postings_file = io.open(postings_path, 'rb')
 	postings_sizes = pickle.load(postings_file)
+	starting_byte_offset = postings_file.tell()
+
+	index_of_term = dictionary.index("bill")
+	posting_offset = 0 if index_of_term - 1 < 0 else postings_sizes[index_of_term - 1]
+
+	print(pickle.load(postings_file))
+	print(pickle.load(postings_file))
+	postings_file.seek(starting_byte_offset + posting_offset, 0)
+	print(pickle.load(postings_file))
+	postings_file.seek(starting_byte_offset + posting_offset, 0)
+	print(pickle.load(postings_file))
 
 	with io.open(query_path, 'r') as f:
-		queries = f.readlines()
-
-	postings_file.close()
+		# remove \n while readlines
+		queries = [line[:-1] for line in f.readlines()]
 
 	stemmer = PorterStemmer()
 
 	for query in queries:
-		print('QUERY RESULT')
-		print(postings[stemmer.stem(query)])
+		print('***QUERY RESULT***')
+		# encode to non-unicode
+		stem = stemmer.stem(query.encode('ascii'))
+		index_of_term = dictionary.index(stem)
+		print(index_of_term)
+
+		# skip if not found
+		if index_of_term < 0:
+			print(stem, 'not in dictionary')
+			continue
+
+		# calculate byte offset
+		print(0 if index_of_term - 1 < 0 else postings_sizes[index_of_term - 1])
+		posting_offset = 0 if index_of_term - 1 < 0 else postings_sizes[index_of_term - 1]
+		byte_offset = starting_byte_offset + posting_offset
+		print(byte_offset)
+		postings_file.seek(byte_offset, 0)
+		print(pickle.load(postings_file))
+
+	postings_file.close()
