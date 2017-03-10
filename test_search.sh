67268e2303927e63cfe124e43a0b2f096694b211
Will Ho <ikaruswill.ho@gmail.com>
2017-03-10 22:55:56 +0800

    Update test_search script to use queries instead of queries.txt

--- test_search.sh
+++ test_search.sh
@@ -1,1 +1,1 @@
-python search.py -d dictionary.txt -p postings.txt -q queries.txt -o output.txt -l lengths.txt
+python search.py -d dictionary.txt -p postings.txt -q queries -o output.txt -l lengths.txt
