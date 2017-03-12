d1da3d31051d7207c9753ae9946ddc5b472a58cf
Will Ho <ikaruswill.ho@gmail.com>
2017-03-12 14:12:56 +0800

    Update test scripts to be compatible with python aliases

--- test_index.sh
+++ test_index.sh
@@ -1,1 +1,10 @@
-python3 index.py -i ~/nltk_data/corpora/reuters/training -d dictionary.txt -p postings.txt -l lengths.txt
+# /bin/sh
+
+if hash python3 >/dev/null
+then
+	py_cmd="python3"
+else
+	py_cmd="python"
+fi
+
+$py_cmd index.py -i ~/nltk_data/corpora/reuters/training -d dictionary.txt -p postings.txt -l lengths.txt
--- test_search.sh
+++ test_search.sh
@@ -1,1 +1,10 @@
-python3 search.py -d dictionary.txt -p postings.txt -q queries.txt -o output.txt -l lengths.txt
+# /bin/sh
+
+if hash python3 >/dev/null
+then
+	py_cmd="python3"
+else
+	py_cmd="python"
+fi
+
+$py_cmd search.py -d dictionary.txt -p postings.txt -q queries.txt -o output.txt -l lengths.txt
